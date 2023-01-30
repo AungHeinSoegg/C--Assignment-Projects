be481bf0c73a2a54fc02c0be53a9da14029d2cbd
AUNG HEIN SOE <120496646+AungHeinSoegg@users.noreply.github.com>
2023-01-30 21:49:55 +0630

    step 1
    အခုcode လေးကို copy လုပ်ပြီးတော့ဘဲဖြစ်ဖြစ် ကိုယ်တိုင်လိုက်ရေးပြီးတော့ဘဲဖြစ်
    ယူပြီး run ပေးကြည့်ပါ..
    screen short လေးပါတွဲတင်ပေးပါတယ်ခင်ဗျာ...

---
+++ step1member.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+#include<cstdlib>
+#include<conio.h>
+
+using namespace std;
+
+class member
+
+{
+private:
+
+public:
+	void addMember();
+};
+
+void member::addMember()
+{
+
+	int choice;
+	char x;
+	system("cls");
+	
+	cout << "Student Management System" << endl;
+	cout << "<<<<<<<<--------->>>>>>>>" << endl;
+}
+
+int main()
+{
+	member m;
+	m.addMember();
+}
+
