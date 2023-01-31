9d2cf82e5b184ad07858d109b59083cb095cd2c8
AUNG HEIN SOE <120496646+AungHeinSoegg@users.noreply.github.com>
2023-02-01 00:09:58 +0630

    step 4  case 4: exit program and output text file
    step 4 မှာ case 4: အတွက် exit(0); exit program အတွက် ဖြည့်စွက်ရေးသာထားပါတယ်.။ နောက်တစ်ခုကတော့ void member::insert() ထဲမှာ member တွေရဲ့အချက်အလက်တွေကို .text file အနေနဲ့
    output ထုတ်ပေးနိုင်အောင် write file လုပ်မည့် code တွေထည့်ရေးထားပါတယ်/ void member::insert() ရဲ့အောက်ဆုံးနားလေးမှာပါ//

---
+++ step4 switch (choice).cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+#include<cstdlib>
+#include<conio.h>
+
+using namespace std;
+	
+class member
+
+{
+private:
+	string name, id_no, type, point;
+
+public:
+	void addMember();
+	void insert();
+	void showDetail();
+};
+
+void member::addMember()
+{
+	mainmenu:
+	int choice;
+	char x;
+	system("cls");
+	
+	cout << "Student Management System" << endl;
+	cout << "<<<<<<<<--------->>>>>>>>" << endl;
+	cout << "1. Add New Member" << endl;
+	cout << "2. Modifies an EXisting Member" << endl;
+	cout << "3. Display The Member Details From The File" << endl;
+	cout << "4. Exit" << endl;
+	
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	cout << " Choose No For Confirm Your Option:[ 1/ 2/ 3/ 4]" << endl;
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	cout << "Enter Your Choose: ";
+	cin >> choice;
+	
+	switch(choice)
+	{
+		case 1://add new member detail and adding another member for next in case 1.
+		do
+		{
+			member::insert();
+			member::showDetail();
+			
+				cout << "Add Anothe Member Detail (Y,N): ";
+				cin >> x;
+			break;
+		
+		case 4:
+			exit(0);
+			break;
+			
+			
+		} 
+		while (x == 'y' || x == 'Y');
+		system("PAUSE"); 
+		break;
+		
+		default:
+		cout << "\n\t\t\t Invaild choice...Please Try Again...";
+		system("PAUSE");
+		break;
+	}
+	goto mainmenu;
+
+}
+void member::insert() // Adding Member Detail.
+{
+	system("cls");
+	fstream file;
+	cout << "=====> ADD NEW MEMBER <=====" << endl;
+
+	cout << "Enter Member Name: ";
+	cin >> name;
+	cout << "Enter Member ID  : ";
+	cin >> id_no;
+	cout << "Enter Member's Type(Ordinary/ Sailver/ Gold): ";
+	cin >> type;
+	cout << "Enter Member Credit Point: ";
+	cin >> point;
+	file.open("MemberDetail.txt", ios::app | ios::out); // Member datila Text Fil Creating.
+	file << " " << name << " " << id_no << " " << type << " " << point << "\n";
+	file.close();
+}
+void member::showDetail()
+{
+	system("cls");
+	fstream file;
+	cout << "=====> WELCOME <=====" << endl;
+	cout<<"Member Name 	: "<<name<<endl;
+	cout<<"Member Number: "<<id_no<<endl;
+	cout<<"Member Type	: "<<type<<endl;
+	cout<<"Credit Point	: "<<point<<endl;
+	cout<<"......................." << endl;	
+}	
+int main()
+{
+	member m;
+	m.addMember();
+	m.insert();
+	m.showDetail();
+}
+
